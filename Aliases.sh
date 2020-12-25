c65f309fb2d2f5abbc0dfc0d92c810595057b1b5
nickbailuc <35620734+nickbailuc@users.noreply.github.com>
2020-12-25 11:41:19 -0500

    Add files via upload

--- Aliases.sh
+++ Aliases.sh
@@ -2,35 +2,31 @@
 
  alias a="emacs -nw $HOME/.bash_aliases; rm $HOME/Bash/Aliases.sh~ 2> /dev/null; exit"
  alias b="sudo emacs -nw /etc/default/grub; sudo rm /etc/default/grub~ 2> /dev/null
-          sudo update-grub; exit"
+             sudo update-grub; exit"
        c() { echo $(($@)); }
  alias d="dpkg -l | grep"
        e() { T=$(ls -l "$@" | awk '{print $3}'); if [ "$T" == "root" ]
-       then sudo emacs -nw "$@"; sudo rm "$@"~
-       else emacs -nw "$@"; rm "$@"~; exit; fi; }
+             then sudo emacs -nw "$@"; sudo rm "$@"~ 2> /dev/null
+             else emacs -nw "$@"; rm "$@"~; exit; fi; }
  alias f="apt search"
  alias g="gcc *.c && ./a.out"
        h() { if [[ "$@" == "" ]]; then history; else history | grep "$@"; fi; }
  alias i="sudo apt install"
  alias j="javac *.java"
 #alias k=""
-       l() { cd $HOME/"$1"; }
+ alias l="watch -n 1 sensors"
  alias m="man"
 #alias n=""
  alias o="octave-cli"
  alias p="poweroff"
-#alias q=""
+ alias q="clear"
  alias r="reboot"
  alias s="apt show"
  alias t="time"
  alias u="sudo apt update; sudo apt install -f; sudo apt full-upgrade
-          sudo apt autoremove; sudo apt clean; sudo apt autoclean;"
+             sudo apt autoremove -y; sudo apt clean; sudo apt autoclean;"
 #alias v=""
  alias w="wget ipinfo.io -O - 2> /dev/null | grep :"
-#alias x=""
+ alias x="chmod +x *.sh *.AppImage 2> /dev/null"
 #alias y=""
 #alias z=""
-
- alias ch="chmod +x *.sh *.AppImage 2> /dev/null"
- alias cl="clear"
- alias lm="watch -n 0.1 sensors"
